30f5c0ff680ffc39222cc74a6769e5e6895307f6
Hoang Hung <hoanghung231004@gmail.com>
2025-09-22 23:22:48 +0700

    vid 30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,8 +1,10 @@
 const MyComponent = () => {
     return (
-        <div>
-            Hoang Quoc Hung HE180380
-        </div>
+        <>
+            <div className="box">
+                Hoang Quoc Hung HE180380
+            </div>
+        </>
     );
 }
 export default MyComponent;
